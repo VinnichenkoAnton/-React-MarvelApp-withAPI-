1e58b1fe438f9d27703b448914d9ed9baffcbe5f
Anton V <antonnugabest@gmail.com>
2022-08-09 13:22:29 +0300

    CharList modified. Add ref for each li in array of chars

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -65,12 +65,20 @@ class CharList extends Component {
         })
     }
 
-    onFocus = () => {
-        this.focusRef.current.className = 'newClass';
+    itemRefs = [];
+
+    setRef = (li) => {
+        this.itemRefs.push(li);
+    }
+
+    focusOnItem = (id) => {
+        this.itemRefs.forEach(item => item.classList.remove('char__item_selected'));
+        this.itemRefs[id].classList.add('char__item_selected');
+        this.itemRefs[id].focus();
     }
 
     renderItems(arr) {
-        const items = arr.map((item) => {
+        const items = arr.map((item, i) => {
             let imgStyle = { 'objectFit': 'cover' };
             if (item.thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
                 imgStyle = { 'objectFit': 'unset' };
@@ -78,9 +86,18 @@ class CharList extends Component {
 
             return (
                 <li key={item.id}
-                    ref = {this.focusRef}
-                    onClick={() => {this.props.onCharSelected(item.id);
-                                    this.onFocus()}}
+                    tabIndex = {0}
+                    ref = {this.setRef}
+                    onClick={() => {
+                        this.props.onCharSelected(item.id);
+                        this.focusOnItem(i)
+                    }}
+                    onKeyPress = {(e) => {
+                        if(e.key === ' ' || e.key === 'Enter') {
+                            this.props.onCharSelected(item.id);
+                            this.focusOnItem(i);
+                        }
+                    }}
                     className="char__item">
                     <img src={item.thumbnail} alt={item.name} style={imgStyle} />
                     <div className="char__name">{item.name}</div>
